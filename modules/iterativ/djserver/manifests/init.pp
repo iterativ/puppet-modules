d76780859f8d384d1de63901ec7ecb7593f66a9a
Daniel Egger <daniel.egger@gmail.com>
2016-09-15 18:51:08 +0200

    keep ssh agent forwarding for sudo

--- modules/iterativ/djserver/manifests/init.pp
+++ modules/iterativ/djserver/manifests/init.pp
@@ -11,6 +11,35 @@ class djserver {
     ensure => installed
   }
 
+  # keep ssh agent forward for sudo
+  file { '/etc/sudoers.d/99-keep-ssh-auth-sock-env':
+    ensure  => present,
+    content => template("djserver/sudo_keep_ssh_auth_sock.erb")
+  }
+
+  file { '/root/.ssh/known_hosts':
+    ensure  => 'present',
+    replace => 'no',
+    content => "",
+    mode    => '0644',
+  }
+
+  exec { "add_github_key":
+    path => ["/usr/bin/", "/bin/"],
+    command => "ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts",
+    user => root,
+    unless => "/bin/grep github.com /root/.ssh/known_hosts",
+    require => File['/root/.ssh/known_hosts']
+  }
+
+  exec { "add_bitbucket_key":
+    path => ["/usr/bin/", "/bin/"],
+    command => "ssh-keyscan -t rsa bitbucket.org >> /root/.ssh/known_hosts",
+    user => root,
+    unless => "/bin/grep bitbucket.org /root/.ssh/known_hosts",
+    require => File['/root/.ssh/known_hosts']
+  }
+
   service { "ntp":
     ensure  => running,
     enable  => true,
