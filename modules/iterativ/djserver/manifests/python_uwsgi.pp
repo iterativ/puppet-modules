46833ac9741b1e0af43910d0ecea905b3f71989d
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2017-10-30 16:49:59 +0100

    remove python2, remove /etc/init.d/uwsgi (systemd used instead)

--- modules/iterativ/djserver/manifests/python_uwsgi.pp
+++ modules/iterativ/djserver/manifests/python_uwsgi.pp
@@ -2,23 +2,12 @@ class djserver::python_uwsgi {
 
   require djserver
 
-  package { ['python', 'python-dev', 'python-pip', 'python-setuptools', 'python-virtualenv']:
-    ensure => installed
-  }
-
   package { ['python3', 'python3-dev', 'python3-pip', 'python3-setuptools', 'python3-virtualenv']:
     ensure => installed
   }
 
-# TODO: still needed for 2.7??
-  file { '/usr/lib/python2.7/decimal.py':
-    ensure  => present,
-    require => Package['python'],
-    content => template("djserver/fix_decimal.py")
-  }
-
-# make python semaphores workings:
-# see: http://stackoverflow.com/questions/2009278/python-multiprocessing-permission-denied
+  # make python semaphores workings:
+  # see: http://stackoverflow.com/questions/2009278/python-multiprocessing-permission-denied
   file_line { 'shm mountable':
     line => 'none /dev/shm tmpfs rw,nosuid,nodev,noexec 0 0',
     path => '/etc/fstab',
@@ -29,9 +18,15 @@ class djserver::python_uwsgi {
     require => File_line['shm mountable'],
   }
 
-# uwsgi
+  # uwsgi
   package { ['uwsgi', 'uwsgi-plugin-python']:
     ensure => present
+  }
+
+  # get rid of the default site
+  file { '/etc/init.d/uwsgi':
+    ensure  => absent,
+    require => Package['uwsgi']
   }
 
 }
