076d76ea98a19d4d1a7fe3e1d9766a56e9ff1e5b
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2017-10-31 21:46:11 +0100

    another try

--- modules/iterativ/djserver/manifests/python_uwsgi.pp
+++ modules/iterativ/djserver/manifests/python_uwsgi.pp
@@ -2,20 +2,20 @@ class djserver::python_uwsgi {
 
   require djserver
 
-  package { ['python', 'python-dev', 'python-pip', 'python-setuptools', 'python-virtualenv']:
-    ensure => installed
-  }
+  #package { ['python', 'python-dev', 'python-pip', 'python-setuptools', 'python-virtualenv']:
+  #  ensure => installed
+  #}
 
   package { ['python3', 'python3-dev', 'python3-pip', 'python3-setuptools', 'python3-virtualenv']:
     ensure => installed
   }
 
   # TODO: still needed for 2.7??
-  file { '/usr/lib/python2.7/decimal.py':
-    ensure  => present,
-    require => Package['python'],
-    content => template("djserver/fix_decimal.py")
-  }
+  #file { '/usr/lib/python2.7/decimal.py':
+  #  ensure  => present,
+  #  require => Package['python'],
+  #  content => template("djserver/fix_decimal.py")
+  #}
 
   # make python semaphores workings:
   # see: http://stackoverflow.com/questions/2009278/python-multiprocessing-permission-denied
